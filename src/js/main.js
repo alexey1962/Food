9d795ece2fb8ae95497dd530e70fd5a2237b1033
Alexey <a.kashutin.dev@gmail.com>
2024-07-23 22:15:25 +0500

    adding classes of rest

--- src/js/main.js
+++ src/js/main.js
@@ -108,28 +108,34 @@ window.addEventListener('DOMContentLoaded', () => {
   //Классы для карточек
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src
       this.alt = alt
       this.title = title
       this.descr = descr
       this.price = price
       this.parentSelector = document.querySelector(parentSelector)
+      this.classes = classes
     }
 
     render() {
       const element = document.createElement('div')
+      if(this.classes.length === 0) {
+        this.element = 'menu__item'
+        element.classList.add(this.element)
+      } else {
+        this.classes.forEach(className => element.classList.add(className))
+      }
+
       element.innerHTML = `
-        <div class="menu__item">
-          <img src=${this.src} alt=${this.alt}>
-          <h3 class="menu__item-subtitle">${this.title}</h3>
-          <div class="menu__item-descr">${this.descr}</div>
-          <div class="menu__item-divider"></div>
-          <div class="menu__item-price">
-            <div class="menu__item-cost">Цена:</div>
-            <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
-          </div>
-      </div>
+        <img src=${this.src} alt=${this.alt}>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+          <div class="menu__item-cost">Цена:</div>
+          <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+        </div>
       `
       this.parentSelector.append(element)
     }
@@ -141,7 +147,7 @@ window.addEventListener('DOMContentLoaded', () => {
     'Меню "Фитнес"',
     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
     229,
-    '.menu .container'
+    '.menu .container',
   ).render()
   new MenuCard(
     "img/tabs/elite.jpg",
@@ -149,7 +155,8 @@ window.addEventListener('DOMContentLoaded', () => {
     'Меню “Премиум”',
     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
     550,
-    '.menu .container'
+    '.menu .container',
+    'menu__item'
   ).render()
   new MenuCard(
     "img/tabs/post.jpg",
@@ -157,6 +164,7 @@ window.addEventListener('DOMContentLoaded', () => {
     'Меню "Постное"',
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     430,
-    '.menu .container'
+    '.menu .container',
+    'menu__item'
   ).render()
 })
